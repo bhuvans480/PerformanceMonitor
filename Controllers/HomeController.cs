238907731848a5f4ce9dfa2cfa409161a4b07228
bhuvaneshwaran005 <bhuvaneshwaran.s@prodapt.com>
2022-05-27 03:18:45 +0530

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using PerformanceMonitor.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PerformanceMonitor.Controllers
+{
+    public class HomeController : Controller
+    {
+        SQLiteConnection conn = new SQLiteConnection("Data Source = D:\\DOT NET\\Sqlite\\database.db; Version=3;");
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetData()
+        {
+            try
+            {
+                List<PerformanceModel> lst = new List<PerformanceModel>();
+                conn.Open();
+                SQLiteCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT * FROM PerformanceMonitor";
+
+                SQLiteDataReader reader = cmd.ExecuteReader();
+                
+                while (reader.Read())
+                {
+                    PerformanceModel obj = new PerformanceModel();
+                    obj.dateTime = reader[0].ToString();
+                    obj.cpuUsage = Decimal.Parse(reader[1].ToString());
+                    obj.memoryUsage = Decimal.Parse(reader[2].ToString());
+                    lst.Add(obj);
+                    
+                }
+
+                //var filteredLst = lst.OrderByDescending(x => x.dateTime);
+
+                var latest = lst[lst.Count - 1];
+
+                return this.Json(latest, JsonRequestBehavior.AllowGet);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return HttpNotFound();
+            }
+
+        }
+    }
+}
---
+++ Models/PerformanceModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PerformanceMonitor.Models
+{
+    public class PerformanceModel
+    {
+        public string dateTime { get; set; }
+        public decimal cpuUsage { get; set; }
+        public decimal memoryUsage { get; set; }
+
+    }
+}
